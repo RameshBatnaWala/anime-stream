7dd80440e89fc4cbafe747ae46d9724ebdcfa4a8
KspPaul <1paulengelhardt@gmail.com>
2019-09-03 22:54:31 +0200

    Player design updated

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -6,6 +6,7 @@
         
     <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
     <link href="/css/Video.css" rel="stylesheet">
+    <link href="/css/VideoRedesign.css" rel="stylesheet">
     <script src="https://unpkg.com/video.js/dist/video.js"></script>
     <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
     </head>
