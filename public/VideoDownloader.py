624a2b84ff8ffa6225405aee5df139904132a11b
KspPaul <1paulengelhardt@gmail.com>
2019-09-04 21:21:21 +0200

    x

--- public/VideoDownloader.py
+++ public/VideoDownloader.py
@@ -37,7 +37,7 @@ def GetVideoDirAndConvert(showName, episode):
     print(vName);
     rename = "mv out storage/'"+vName+"'";
     os.system(rename);
-    os.system("rm -rf Downloads/"+GetFolderName()+"")
+    os.system("rm -rf Downloads/*")
     return vName;
     
 def SaveToDatabase(SeriesID, showName, episode, cursor, fileName, Database):
